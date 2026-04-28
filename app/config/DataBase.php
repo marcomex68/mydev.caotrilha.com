12937724acd9001a1dab6de0e3268eec59d4b6e0
MarcoFigueiredoAlves <37076@esjaloures.org>
2026-04-28 15:04:05 +0100

    alterei

---
+++ app/config/DataBase.php
@@ -0,0 +1,15 @@
+<?php
+class DataBase
+{
+    private $host = 'localhost';
+    private $db_name = 'caotrilha2';
+    private $username = 'root';
+    private $password = '';
+
+    public function connect()
+    {
+        $conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    }
+}
